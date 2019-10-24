512eeef08276094756bbd6a5c5679f0d020b3e9f
github username <github email address>
2019-10-24 16:21:27 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,34 @@
+// --------------------------------------------------------------------------
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+  // return state
+  // console.log(state);
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count
+}
 
-changeState(state, action)
+// call the render function
+
+
+
+dispatch({type: 'INCREASE_COUNT'})
+  // => 1
+// dispatch({type: 'INCREASE_COUNT'})
+//   // => 2
+// dispatch({type: 'INCREASE_COUNT'})
+
+// --------------------------------------------------------------------------
